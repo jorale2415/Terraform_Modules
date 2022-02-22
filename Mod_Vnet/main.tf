1d5a40dcc61752dcea476ce86ebb97beb296f5ac
Jordan Alex <60239297+jorale2415@users.noreply.github.com>
2022-02-22 07:55:42 -0600

    .

--- Mod_Vnet/main.tf
+++ Mod_Vnet/main.tf
@@ -1,20 +1,20 @@
 terraform {
   required_version = ">= 1.1.5"
 }
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "my_vnet" {
   name                = var.vnet_name
   location            = var.vnet_location
   resource_group_name = var.vnet_rg_name
   address_space       = var.vnet_address_space
   dns_servers         = var.vnet_dns_servers
 }
-resource "azurerm_subnet" "MyResource" {
+resource "azurerm_subnet" "my_subnets" {
    count = var.number_of_subnets
    name = "subnet-${count.index + 1}"
    resource_group_name = var.vnet_rg_name
    virtual_network_name = var.vnet_name
    address_prefix = cidrsubnet(element(var.vnet_address_space,0), 8, "${count.index}")
    depends_on = [
-     azurerm_virtual_network.example
+     azurerm_virtual_network.my_vnet
    ]
 }
