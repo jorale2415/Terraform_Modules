9684b2f504fe0273e1347bbd0e5ce96f0fe6b5f1
Jordan Alex <60239297+jorale2415@users.noreply.github.com>
2022-02-24 16:44:32 -0600

    .

---
+++ Mod_Application_Gateway/variables.tf
@@ -0,0 +1,15 @@
+variable "team" {
+  type = string
+}
+variable "resource_group" {
+  type = string
+}
+variable "vnet" {
+  type = string
+}
+variable "subnet_name" {
+  type = string
+}
+variable "address_prefix" {
+  type = string
+}
