e69800eee2d390314a469d3e31af3fe4a2a93c4e
Jordan Alex <60239297+jorale2415@users.noreply.github.com>
2022-02-24 11:56:07 -0600

    .

--- Mod_Vnet_Peering/main.tf
+++ Mod_Vnet_Peering/main.tf
@@ -30,17 +30,3 @@ resource "azurerm_virtual_network_peering" "vnet_peering_SR_PG" {
    virtual_network_name = data.azurerm_virtual_network.secondary_vnet.name
    remote_virtual_network_id = data.azurerm_virtual_network.primary_vnet.id
 }
-/*
-resource "azurerm_virtual_network_peering" "Primary_vnet_Peered_Secondary_vnet" {
-   name = "${var.team}-${var.region1}-peered-${var.region2}"
-   resource_group_name = data.azurerm_resource_group.rg1.name
-   virtual_network_name = data.azurerm_virtual_network.primary_vnet.name
-   remote_virtual_network_id = data.azurerm_virtual_network.secondary_vnet.id
-}
-resource "azurerm_virtual_network_peering" "Secondary_vnet_Peered_Primary_vnet" {
-   name = "${var.team}-${var.region2}-peered-${var.region1}"
-   resource_group_name = data.azurerm_resource_group.rg2.name
-   virtual_network_name = data.azurerm_virtual_network.secondary_vnet.name
-   remote_virtual_network_id = data.azurerm_virtual_network.primary_vnet.id
-}
-*/
