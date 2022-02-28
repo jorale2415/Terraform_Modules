7d033b0f8a9e0029118b5e5b68a8fa747d050224
Jordan Alex <60239297+jorale2415@users.noreply.github.com>
2022-02-28 08:40:51 -0600

    .

--- Mod_Application_Gateway/main.tf
+++ Mod_Application_Gateway/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "${var.team}application-gateway-pip"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 #&nbsp;since these variables are re-used - a locals block makes this more maintainable
