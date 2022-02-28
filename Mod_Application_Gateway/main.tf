94fdcf3557406aad65e27578bffebbabc5bb3d7d
Jordan Alex <60239297+jorale2415@users.noreply.github.com>
2022-02-28 11:10:47 -0600

    .

--- Mod_Application_Gateway/main.tf
+++ Mod_Application_Gateway/main.tf
@@ -80,7 +80,6 @@ resource "azurerm_application_gateway" "network" {
   backend_http_settings {
     name                  = local.http_setting_name
     cookie_based_affinity = "Disabled"
-    path                  = "/*"
     port                  = 80
     protocol              = "Http"
     request_timeout       = 60
