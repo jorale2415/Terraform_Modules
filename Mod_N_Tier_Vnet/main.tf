f6543843ddf7dc7655dcbd3c5c2cdb025ad01189
Jordan Alex <60239297+jorale2415@users.noreply.github.com>
2022-03-01 11:15:37 -0600

    .

--- Mod_N_Tier_Vnet/main.tf
+++ Mod_N_Tier_Vnet/main.tf
@@ -59,6 +59,17 @@ resource "azurerm_network_security_group" "bastionnsg" {
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
+    security_rule {
+        name                       = "AppGateway80"
+        priority                   = 1003
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 
     security_rule {
         name                       = "OutboundVirtualNetwork"
